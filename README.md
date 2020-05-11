e18bb5ee728b3622fc57418d05214f426a816dd3
Brakebein <bruschie@hotmail.com>
2020-05-11 17:49:26 +0200

    index.js

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-neo4j-request
+# neo4j-request
+
+A simplified interface for [neo4j-driver](https://github.com/neo4j/neo4j-javascript-driver) that extracts and converts the queried properties from the records.
+
+## Installation
+
+Via npm:
+
+    npm install brakebein/neo4j-request
+
+## Usage
+
+Import:
+
+```javascript
+const neo4j = require('neo4j-request');
+// or
+import * as neo4j from 'neo4j-request';
+```
+
+Before doing any transaction, the neo4j-driver needs to be initialized once.
+The driver instance is set globally, such that you can import neo4j in any other module without the need to initialize again.
+For detailed `options` refer to [neo4j-driver](https://github.com/neo4j/neo4j-javascript-driver).
+
+```javascript
+neo4j.init(url, user, password, database = 'neo4j', options = {disableLosslessIntegers: true});
+```
+
+Simple read transaction:
+
+```javascript
+const query = `
+  MATCH (p:Person {name: $name)-[:HAS_ADDRESS]->(add:Address)
+  RETURN p.name AS name, add AS address
+`;
+
+const params = {
+  name: 'Alex'
+};
+
+try {
+  
+  const results = await neo4j.readTransaction(query, params);
+  console.log(results);
+  
+} catch (e) {
+  
+  // handle error
+  
+}
+
+// console.log(results)
+// [
+//   {
+//     name: 'Alex',
+//     address: {
+//       street: 'Alexanderplatz',
+//       number: '1',
+//       ZIP: '10178',
+//       town: 'Berlin'
+//     }
+//   }
+// ]
+```
---
+++ index.js
@@ -0,0 +1,250 @@
+const neo4j = require('neo4j-driver');
+
+let config = {};
+
+/**
+ * @type {Driver}
+ */
+let driver;
+
+/**
+ * Initialize Neo4j driver instance.
+ * @param url {string}
+ * @param user {string}
+ * @param password {string}
+ * @param database {string=} Database, default: 'neo4j'
+ * @param options {Config=} neo4j-driver config options
+ */
+function init(url, user, password, database = 'neo4j', options = {}) {
+
+  config.url = url;
+  config.auth = neo4j.auth.basic(user, password);
+  config.database = database;
+  config.options = Object.assign({disableLosslessIntegers: true}, options);
+
+  driver = neo4j.driver(url, config.auth, config.options);
+
+  driver.verifyConnectivity()
+    .then(serverInfo => {
+      console.log('Neo4j Server:', serverInfo);
+    })
+    .catch(reason => {
+      console.error('Neo4j driver instantiation failed', reason);
+    });
+
+  process.on('exit', async () => {
+    await driver.close();
+  });
+
+}
+
+/**
+ * READ transaction without modifying database.
+ * @param query {string}
+ * @param params {Object=}
+ * @return {Promise<Object[]>}
+ */
+async function readTransaction(query, params = {}) {
+
+  if (!driver) {
+    return Promise.reject('Neo4j driver not initialized!');
+  }
+
+  const session = driver.session({
+    database: config.database,
+    defaultAccessMode: neo4j.session.READ
+  });
+
+  try {
+
+    const result = await session.readTransaction(tx => tx.run(query, params));
+    return extractBoltRecords(result.records);
+
+  } catch (e) {
+
+    return Promise.reject(e);
+
+  } finally {
+
+    await session.close();
+
+  }
+
+}
+
+/**
+ * WRITE transaction that modifies database.
+ * @param query {string}
+ * @param params {Object=}
+ * @return {Promise<Object[]>}
+ */
+async function writeTransaction(query, params = {}) {
+
+  if (!driver) {
+    return Promise.reject('Neo4j driver not initialized!');
+  }
+
+  const session = driver.session({
+    database: config.database
+  });
+
+  try {
+
+    const result = await session.writeTransaction(tx => tx.run(query, params));
+    return extractBoltRecords(result.records);
+
+  } catch (e) {
+
+    return Promise.reject(e);
+
+  } finally {
+
+    await session.close();
+
+  }
+
+}
+
+/**
+ * Call multiple statements in one transaction.
+ * @param statements {Array<{statement: string, parameters: Object<key, *>}>}
+ * @return {Promise<Object[][]>}
+ */
+async function multipleStatements(statements) {
+
+  if (!driver) {
+    return Promise.reject('Neo4j driver not initialized!');
+  }
+
+  const session = driver.session({
+    database: config.database
+  });
+
+  const txc = session.beginTransaction();
+
+  try {
+
+    const results = [];
+
+    for (const s of statements) {
+      const result = await txc.run(s.statement, s.parameters);
+      results.push(extractBoltRecords(result.records));
+    }
+
+    await txc.commit();
+
+    return results;
+
+  } catch (e) {
+
+    await txc.rollback();
+    return Promise.reject(e);
+
+  } finally {
+
+    await session.close();
+
+  }
+
+}
+
+/**
+ * Extract and convert records returned by neo4j-driver.
+ * @param data {Record[]}
+ * @return {Object[]}
+ */
+function extractBoltRecords(data) {
+
+  if (!data) {
+    return [];
+  }
+
+  if (!Array.isArray(data)) {
+    return data;
+  }
+
+  return data.map(record => {
+    const obj = {};
+    record.keys.forEach(key => {
+      obj[key] = convertValues(record.get(key));
+    })
+    return obj;
+  });
+
+}
+
+function convertValues(value) {
+
+  // neo4j integers
+  if (neo4j.isInt(value)) {
+    if (neo4j.integer.inSafeRange(value)) {
+      return value.toNumber();
+    } else {
+      return value.toString();
+    }
+  }
+
+  // neo4j Node object
+  if (value instanceof neo4j.types.Node) {
+    value = value.properties;
+  }
+
+  // recursive
+  if (Array.isArray(value)) {
+    return value.map(v => convertValues(v));
+  }
+
+  if (typeof value === 'object' && value !== null) {
+    for (const key of Object.keys(value)) {
+      value[key] = convertValues(value[key]);
+    }
+  }
+
+  return value;
+
+}
+
+/**
+ * Look for empty arrays returned by Neo4j and clean them, if there is `null` inside.
+ *
+ * Sometimes, if the cypher query contains `OPTIONAL MATCH node` in combination with  `collect({key: node.value}) AS values`, the resulting array may be filled with one object with `null` values: `[{key: null}]`. This method reduces the array to `[]` by calling `removeEmptyArrays(data, 'values', 'key')`.
+ *
+ * @param data {*[]}
+ * @param arrayKey {string} Property key of the array to check
+ * @param checkKey {string} Property key of first array element to check against `null`
+ * @return {*[]}
+ */
+function removeEmptyArrays(data, arrayKey, checkKey) {
+
+  for (let i = 0, l = data.length; i < l; i++) {
+
+    if (data[i][arrayKey] && Array.isArray(data[i][arrayKey]) && data[i][arrayKey][0]) {
+
+      if (data[i][arrayKey][0][checkKey] === null) {
+        data[i][arrayKey] = [];
+      }
+
+    }
+
+    for (let key in data[i]) {
+
+      if (data[i].hasOwnProperty(key) && Array.isArray(data[i][key])) {
+        removeEmptyArrays(data[i][key], arrayKey, checkKey);
+      }
+
+    }
+
+  }
+
+  return data;
+
+}
+
+module.exports = {
+  init,
+  readTransaction,
+  writeTransaction,
+  multipleStatements,
+  extractBoltRecords,
+  removeEmptyArrays
+};
